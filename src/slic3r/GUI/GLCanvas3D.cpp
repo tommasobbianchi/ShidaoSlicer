tommasobbianchi/ShidaoSlicer

commit a725bdf56b77cbcf2af3bc3b7020ba1591cf3385

SLA gizmo - points are added on leftdown event, not leftup as before

src/slic3r/GUI/GLCanvas3D.cpp
@@ -3445,10 +3445,6 @@ void GLCanvas3D::on_mouse(wxMouseEvent& evt)
             _update_gizmos_data();
             m_dirty = true;
         }
-        else if (evt.LeftDown() && m_gizmos.get_current_type() == Gizmos::SlaSupports && evt.ShiftDown() && m_gizmos.gizmo_event(SLAGizmoEventType::LeftDown, Vec2d(pos(0), pos(1)), evt.ShiftDown()))
-        {
-            // the gizmo got the event and took some action, there is no need to do anything more
-        }
         else if (evt.LeftDown() && !m_selection.is_empty() && m_gizmos.grabber_contains_mouse())
         {
             _update_gizmos_data();
@@ -3463,6 +3459,10 @@ void GLCanvas3D::on_mouse(wxMouseEvent& evt)
             }
 
             m_dirty = true;
+        }
+        else if (evt.LeftDown() && m_gizmos.get_current_type() == Gizmos::SlaSupports && m_gizmos.gizmo_event(SLAGizmoEventType::LeftDown, Vec2d(pos(0), pos(1)), evt.ShiftDown()))
+        {
+            // the gizmo got the event and took some action, there is no need to do anything more
         }
         else if ((selected_object_idx != -1) && evt.RightDown() && m_gizmos.get_current_type() == Gizmos::SlaSupports && m_gizmos.gizmo_event(SLAGizmoEventType::RightDown))
         {
@@ -3621,7 +3621,7 @@ void GLCanvas3D::on_mouse(wxMouseEvent& evt)
 
         m_dirty = true;
     }
-    else if (evt.Dragging() && m_gizmos.get_current_type() == Gizmos::SlaSupports && evt.ShiftDown() && m_gizmos.gizmo_event(SLAGizmoEventType::Dragging, Vec2d(pos(0), pos(1)), evt.ShiftDown()))
+    else if (evt.Dragging() && m_gizmos.get_current_type() == Gizmos::SlaSupports && m_gizmos.gizmo_event(SLAGizmoEventType::Dragging, Vec2d(pos(0), pos(1)), evt.ShiftDown()))
     {
         // the gizmo got the event and took some action, no need to do anything more here
         m_dirty = true;
@@ -3674,11 +3674,13 @@ void GLCanvas3D::on_mouse(wxMouseEvent& evt)
             m_layers_editing.accept_changes(*this);
         }
         else if (evt.LeftUp() && m_gizmos.get_current_type() == Gizmos::SlaSupports && !m_gizmos.is_dragging()
-              && !m_mouse.dragging && m_gizmos.gizmo_event(SLAGizmoEventType::LeftUp, Vec2d(pos(0), pos(1)), evt.ShiftDown()))
+              && !m_mouse.dragging)
         {
-            // the gizmo got the event and took some action, no need to do anything more
+            // in case SLA gizmo is selected, we just pass the LeftUp event and stop processing - neither
+            // object moving or selecting is suppressed in that case
+            m_gizmos.gizmo_event(SLAGizmoEventType::LeftUp, Vec2d(pos(0), pos(1)), evt.ShiftDown());
         }
-        else if ((m_mouse.drag.move_volume_idx != -1) && m_mouse.dragging && m_gizmos.get_current_type() != Gizmos::SlaSupports)
+        else if ((m_mouse.drag.move_volume_idx != -1) && m_mouse.dragging)
         {
             m_regenerate_volumes = false;
             do_move();
@@ -3688,11 +3690,8 @@ void GLCanvas3D::on_mouse(wxMouseEvent& evt)
             post_event(SimpleEvent(EVT_GLCANVAS_MOUSE_DRAGGING_FINISHED));
         }
         else if (evt.LeftUp() && !m_mouse.dragging && (m_hover_volume_id == -1) && !gizmos_overlay_contains_mouse && !m_gizmos.is_dragging()
-              && !is_layers_editing_enabled() && (m_gizmos.get_current_type() != Gizmos::SlaSupports || !m_gizmos.gizmo_event(SLAGizmoEventType::LeftUp, Vec2d(pos(0), pos(1)), evt.ShiftDown())))
+              && !is_layers_editing_enabled())
         {
-            // SLA gizmo cannot be deselected by clicking in canvas area to avoid inadvertent unselection and losing manual changes
-            // that's why the gizmo_event function was called so that the gizmo can refuse the deselection in manual editing mode
-
             // deselect and propagate event through callback
             if (!evt.ShiftDown() && m_picking_enabled && !m_mouse.ignore_up_event)
             {

src/slic3r/GUI/Gizmos/GLGizmoSlaSupports.cpp
@@ -326,39 +326,19 @@ bool GLGizmoSlaSupports::gizmo_event(SLAGizmoEventType action, const Vec2d& mous
             return true;
         }
 
-        // dragging the selection rectangle:
-        if (action == SLAGizmoEventType::Dragging && m_selection_rectangle_active) {
-            m_selection_rectangle_end_corner = mouse_position;
-            return true;
-        }
-
-        // mouse up without selection rectangle - place point on the mesh:
-        if (action == SLAGizmoEventType::LeftUp && !m_selection_rectangle_active && !shift_down) {
-            if (m_ignore_up_event) {
-                m_ignore_up_event = false;
-                return false;
-            }
-
-            int instance_id = m_parent.get_selection().get_instance_idx();
-            if (m_old_instance_id != instance_id)
-            {
-                bool something_selected = (m_old_instance_id != -1);
-                m_old_instance_id = instance_id;
-                if (something_selected)
-                    return false;
-            }
-            if (instance_id == -1)
-                return false;
-
+        // left down without selection rectangle - place point on the mesh:
+        if (action == SLAGizmoEventType::LeftDown && !m_selection_rectangle_active && !shift_down) {
             // If there is some selection, don't add new point and deselect everything instead.
             if (m_selection_empty) {
                 try {
                     std::pair<Vec3f, Vec3f> pos_and_normal = unproject_on_mesh(mouse_position); // don't create anything if this throws
                     m_editing_mode_cache.emplace_back(sla::SupportPoint(pos_and_normal.first, m_new_point_head_diameter/2.f, false), false, pos_and_normal.second);
                     m_unsaved_changes = true;
+                    m_parent.set_as_dirty();
+                    m_wait_for_up_event = true;
                 }
-                catch (...) {      // not clicked on object
-                    return true;   // prevents deselection of the gizmo by GLCanvas3D
+                catch (...) {   // not clicked on object
+                    return false;
                 }
             }
             else
@@ -368,10 +348,7 @@ bool GLGizmoSlaSupports::gizmo_event(SLAGizmoEventType action, const Vec2d& mous
         }
 
         // left up with selection rectangle - select points inside the rectangle:
-        if ((action == SLAGizmoEventType::LeftUp || action == SLAGizmoEventType::ShiftUp)
-          && m_selection_rectangle_active) {
-            if (action == SLAGizmoEventType::ShiftUp)
-                m_ignore_up_event = true;
+        if ((action == SLAGizmoEventType::LeftUp || action == SLAGizmoEventType::ShiftUp) && m_selection_rectangle_active) {
             const Transform3d& instance_matrix = m_model_object->instances[m_active_instance]->get_transformation().get_matrix();
             GLint viewport[4];
             ::glGetIntegerv(GL_VIEWPORT, viewport);
@@ -419,6 +396,28 @@ bool GLGizmoSlaSupports::gizmo_event(SLAGizmoEventType action, const Vec2d& mous
             }
             m_selection_rectangle_active = false;
             return true;
+        }
+
+        // left up with no selection rectangle
+        if (action == SLAGizmoEventType::LeftUp) {
+            if (m_wait_for_up_event) {
+                m_wait_for_up_event = false;
+                return true;
+            }
+        }
+
+        // dragging the selection rectangle:
+        if (action == SLAGizmoEventType::Dragging) {
+            if (m_wait_for_up_event)
+                return true; // point has been placed and the button not released yet
+                             // this prevents GLCanvas from starting scene rotation
+
+            if (m_selection_rectangle_active)  {
+                m_selection_rectangle_end_corner = mouse_position;
+                return true;
+            }
+
+            return false;
         }
 
         if (action == SLAGizmoEventType::Delete) {

src/slic3r/GUI/Gizmos/GLGizmoSlaSupports.hpp
@@ -87,8 +87,7 @@ private:
     bool m_selection_rectangle_active = false;
     Vec2d m_selection_rectangle_start_corner;
     Vec2d m_selection_rectangle_end_corner;
-    bool m_ignore_up_event = false;
-    bool m_combo_box_open = false;  // To ensure proper rendering of the imgui combobox.
+    bool m_wait_for_up_event = false;
     bool m_unsaved_changes = false; // Are there unsaved changes in manual mode?
     bool m_selection_empty = true;
     EState m_old_state = Off; // to be able to see that the gizmo has just been closed (see on_set_state)

commit d0408ee83cf5252a1f2d3b8e1ea308fa179cc07d

SLA gizmo fix: adding points to selection by Shift+click should now work again (was broken by a725bdf)

src/slic3r/GUI/GLCanvas3D.cpp
@@ -3521,6 +3521,10 @@ void GLCanvas3D::on_mouse(wxMouseEvent& evt)
             _update_gizmos_data();
             m_dirty = true;
         }
+        else if (evt.LeftDown() && m_gizmos.get_current_type() == Gizmos::SlaSupports && m_gizmos.gizmo_event(SLAGizmoEventType::LeftDown, Vec2d(pos(0), pos(1)), evt.ShiftDown()))
+        {
+            // the gizmo got the event and took some action, there is no need to do anything more
+        }
         else if (evt.LeftDown() && !m_selection.is_empty() && m_gizmos.grabber_contains_mouse())
         {
             _update_gizmos_data();
@@ -3535,10 +3539,6 @@ void GLCanvas3D::on_mouse(wxMouseEvent& evt)
             }
 
             m_dirty = true;
-        }
-        else if (evt.LeftDown() && m_gizmos.get_current_type() == Gizmos::SlaSupports && m_gizmos.gizmo_event(SLAGizmoEventType::LeftDown, Vec2d(pos(0), pos(1)), evt.ShiftDown()))
-        {
-            // the gizmo got the event and took some action, there is no need to do anything more
         }
         else if ((selected_object_idx != -1) && evt.RightDown() && m_gizmos.get_current_type() == Gizmos::SlaSupports && m_gizmos.gizmo_event(SLAGizmoEventType::RightDown))
         {

src/slic3r/GUI/Gizmos/GLGizmoSlaSupports.cpp
@@ -328,6 +328,10 @@ bool GLGizmoSlaSupports::gizmo_event(SLAGizmoEventType action, const Vec2d& mous
 
         // left down without selection rectangle - place point on the mesh:
         if (action == SLAGizmoEventType::LeftDown && !m_selection_rectangle_active && !shift_down) {
+            // If any point is in hover state, this should initiate its move - return control back to GLCanvas:
+            if (m_hover_id != -1)
+                return false;
+
             // If there is some selection, don't add new point and deselect everything instead.
             if (m_selection_empty) {
                 try {