bambulab/BambuStudio

ENH: add filament change length for color change
Jira: none

Signed-off-by: qing.zhang <qing.zhang@bambulab.com>
Change-Id: I84ab638d6af056ede9b9524d1395eb42fa731150

src/libslic3r/Preset.cpp
@@ -932,7 +932,9 @@ static std::vector<std::string> s_Preset_filament_options {
     "filament_extruder_variant",
     //OrcaSlicer
     "enable_pressure_advance", "pressure_advance", "chamber_temperatures","filament_notes",
-    "filament_long_retractions_when_cut","filament_retraction_distances_when_cut","filament_shrink"
+    "filament_long_retractions_when_cut","filament_retraction_distances_when_cut","filament_shrink",
+    //BBS filament change length while the extruder color
+    "filament_change_length"
 };
 
 static std::vector<std::string> s_Preset_machine_limits_options {

tommasobbianchi/ShidaoSlicer

(cherry picked from commit 9b46d437bad707d38a978bc768e0359979e5fb19)

src/libslic3r/Preset.cpp
@@ -876,7 +876,9 @@ static std::vector<std::string> s_Preset_filament_options {
     "filament_unloading_speed", "filament_unloading_speed_start", "filament_toolchange_delay", "filament_cooling_moves", "filament_stamping_loading_speed", "filament_stamping_distance",
     "filament_cooling_initial_speed", "filament_cooling_final_speed", "filament_ramming_parameters",
     "filament_multitool_ramming", "filament_multitool_ramming_volume", "filament_multitool_ramming_flow", "activate_chamber_temp_control",
-    "filament_long_retractions_when_cut","filament_retraction_distances_when_cut", "idle_temperature"
+    "filament_long_retractions_when_cut","filament_retraction_distances_when_cut", "idle_temperature",
+    //BBS filament change length while the extruder color
+    "filament_change_length"
     };
 
 static std::vector<std::string> s_Preset_machine_limits_options {